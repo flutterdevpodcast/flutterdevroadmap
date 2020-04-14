8112c60ed575cee31c7497edb697ae1b9d6e0943
belenkov <belenkov@surfstudio.ru>
2020-04-14 13:19:47 +0500

    Добавил контент по переменным Добавил контент по типам данных

---
+++ knowledge_base/general/programming_languages/dart/variables/variables.dart
@@ -0,0 +1,19 @@
+const test = "test constant"; //глобальная переменная
+
+class A {
+  var a = 10; //переменная класса
+
+  void main() {
+    int b = 20; //локальна переменная
+    print(b);
+  }
+}
+
+const section = "staff";
+
+class Employee {
+  var name = "Ivan";
+  String surname = "Ivanov";
+  int age = 18;
+  final position = "programmer";
+}
