c7c9d48dd74a1fd1c6973822c33b1e757be755da
belenkov <belenkov@surfstudio.ru>
2020-04-15 12:34:50 +0500

    добавил контент по исключениям

---
+++ assets/roadmap/dart.mm
@@ -0,0 +1,86 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node BACKGROUND_COLOR="#66ff66" CREATED="1578580420713" ID="ID_1661130015" LINK="roadmap.mm" MODIFIED="1586872746459" STYLE="bubble" TEXT="Dart">
+<richcontent TYPE="NOTE"><html>
+  <head>
+
+  </head>
+  <body>
+    <p class="MsoNormal">
+      knowledge_base/general/programming_languages/dart
+    </p>
+  </body>
+</html></richcontent>
+<edge COLOR="#33ff00" STYLE="bezier" WIDTH="2"/>
+<font NAME="SansSerif" SIZE="22"/>
+<icon BUILTIN="button_ok"/>
+<node CREATED="1586758682687" HGAP="41" ID="ID_1911400782" MODIFIED="1586863091316" POSITION="right" TEXT="Data Types" VSHIFT="62"/>
+<node CREATED="1586758803819" HGAP="48" ID="ID_1789061597" MODIFIED="1586863093496" POSITION="right" TEXT="Variables" VSHIFT="8"/>
+<node CREATED="1586759056374" HGAP="44" ID="ID_803403004" MODIFIED="1586861108759" POSITION="right" TEXT="Collections" VSHIFT="-21">
+<node CREATED="1586759066169" HGAP="36" MODIFIED="1586759115705" TEXT="List" VSHIFT="-12"/>
+<node CREATED="1586759086671" HGAP="35" MODIFIED="1586759139215" TEXT="Set" VSHIFT="-3"/>
+<node CREATED="1586759100605" HGAP="32" MODIFIED="1586759138127" TEXT="Map" VSHIFT="8"/>
+</node>
+<node CREATED="1586759170826" FOLDED="true" HGAP="47" ID="ID_1586817399" MODIFIED="1586935765145" POSITION="right" TEXT="Functions" VSHIFT="-72">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      knowledge_base/general/programming_languages/dart/functions
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1586759235555" MODIFIED="1586759239382" TEXT="Parameters"/>
+<node CREATED="1586759242428" MODIFIED="1586759248782" TEXT="Anonymous Functions"/>
+<node CREATED="1586759254545" MODIFIED="1586759257389" TEXT="Inner Function"/>
+<node CREATED="1586759298110" MODIFIED="1586759302292" TEXT="Closure"/>
+</node>
+<node CREATED="1586759344163" HGAP="18" ID="ID_634771983" MODIFIED="1586861108763" POSITION="right" TEXT="Exceptions" VSHIFT="-90">
+<node CREATED="1586759378209" MODIFIED="1586759380161" TEXT="try on catch finally"/>
+<node CREATED="1586759382646" MODIFIED="1586759389920" TEXT="Custom Exceptions"/>
+<node CREATED="1586759398869" MODIFIED="1586759414008" TEXT="&#x41f;&#x440;&#x43e;&#x431;&#x440;&#x43e;&#x441; &#x418;&#x441;&#x43a;&#x43b;&#x44e;&#x447;&#x435;&#x43d;&#x438;&#x439;"/>
+</node>
+<node CREATED="1586759454556" ID="ID_1526799563" MODIFIED="1586861108766" POSITION="right" TEXT="OOP">
+<richcontent TYPE="NOTE"><html>
+  <head>
+
+  </head>
+  <body>
+    <p>
+      knowledge_base/general/programming_languages/dart/variables
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1586759517621" ID="ID_609243094" MODIFIED="1586759522308" TEXT="Classes and Objects"/>
+<node CREATED="1586759782544" ID="ID_1061929821" MODIFIED="1586759786876" TEXT="Constructors"/>
+<node CREATED="1586759792997" ID="ID_710998405" MODIFIED="1586759797283" TEXT="Getters and Setters"/>
+<node CREATED="1586759801991" ID="ID_1128463573" MODIFIED="1586759803443" TEXT="static"/>
+<node CREATED="1586759843456" ID="ID_727226163" MODIFIED="1586759856736" TEXT="Inheritance, encapsulation, polymorphism"/>
+<node CREATED="1586759868507" ID="ID_41008172" MODIFIED="1586759878904" TEXT="Abstract Class, Interface"/>
+<node CREATED="1586759887487" MODIFIED="1586759891896" TEXT="Mixin"/>
+<node CREATED="1586759900214" MODIFIED="1586759904215" TEXT="Extention"/>
+</node>
+<node CREATED="1586759914417" HGAP="19" ID="ID_930289397" MODIFIED="1586861108767" POSITION="right" TEXT="Asynchronous programming" VSHIFT="37">
+<node CREATED="1586847718311" ID="ID_45031648" MODIFIED="1586847723563" TEXT="async/await"/>
+<node CREATED="1586847726475" HGAP="25" ID="ID_885003433" MODIFIED="1586847732267" TEXT="Future" VSHIFT="19"/>
+</node>
+<node CREATED="1586861184749" HGAP="51" ID="ID_932751370" MODIFIED="1586861202745" POSITION="left" TEXT="control flow" VSHIFT="-280">
+<node CREATED="1586861198418" ID="ID_106264531" MODIFIED="1586866599048" TEXT="loops"/>
+<node CREATED="1586861202746" ID="ID_1989484860" MODIFIED="1586866604537" TEXT="conditional operators">
+<richcontent TYPE="NOTE"><html>
+  <head>
+
+  </head>
+  <body>
+    <p>
+      knowledge_base/general/programming_languages/dart/conditional_statements
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+</node>
+</map>
